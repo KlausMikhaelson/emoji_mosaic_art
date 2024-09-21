e845809ed946f1b92211dae1ae9ec1c869f7666c
KlausMikhaelson <satyamsingh5076@gmail.com>
2024-09-20 22:08:17 -0600

    image upload is working now

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
+"use client"
+
+import ImageUpload from "@/components/ImageUpload";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [imagePath, setImagePath] = useState<string | null>(null);
+  const [emojiMosaic, setEmojiMosaic] = useState<string | null>(null);
+
+  async function handleUpload(filePath: string) {
+    setImagePath(filePath);
+    const res = await fetch(`/api/process-image`, {
+      method: "POST",
+      body: JSON.stringify({imagePath: filePath}),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    });
+    const data = await res.json();
+    setEmojiMosaic(data.emojiMosaic);
+  }
+
   return (
     <div>
-      <h1>Image to Art</h1>
-      
+      <h1>Emoji Mosaic Generator</h1>
+      <ImageUpload onUpload={handleUpload} />
+      {imagePath && <img src={`https://hfecpbfntuxaswbitevl.supabase.co/storage/v1/object/public/images/${imagePath}`} alt="uploaded images" />}
+      {emojiMosaic && <img src={`data:image/png;base64,${emojiMosaic}`} alt="emoji mosaic" />}
     </div>
   );
 }
--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,19 +1,39 @@
-// import { useState } from "react";
-// import { supabase } from "@/lib/supabase";
+import { useState } from "react";
+import { supabase } from "@/lib/supabase";
 
-// export default function ImageUpload ({onUpload}) {
-//     const [uploading, setUploading] = useState(false);
+export default function ImageUpload ({onUpload} : {onUpload: (url: string) => void}) {
+    const [uploading, setUploading] = useState(false);
 
-//     async function uploadImage(event: React.ChangeEvent<HTMLInputElement>) {
-//         try {
-//             setUploading(true);
-//             if(!event.target.files || event.target.files.length === 0) {
-//                 throw new Error("You must select an image to upload.");
-//             }
+    async function uploadImage(event: React.ChangeEvent<HTMLInputElement>) {
+        try {
+            setUploading(true);
+            if(!event.target.files || event.target.files.length === 0) {
+                throw new Error("You must select an image to upload.");
+            }
 
-//             const file = event.target.files[0];
-//             const fileExt = file.name.split(".").pop();
-//             const fileName = `${Math.random()}.${fileExt}`;
-//         }
-//     }
-// }
+            const file = event.target.files[0];
+            const fileExt = file.name.split(".").pop();
+            const fileName = `${Math.random()}.${fileExt}`;
+            const filePath = `${fileName}`;
+
+            const {error: uploadError} = await supabase.storage.from("images").upload(filePath, file);
+            if(uploadError) {
+                throw uploadError;
+            }
+
+            onUpload(filePath);
+        } catch (error) {
+            console.error("Error uploading image", error);
+            alert("Error uploading image");
+        } finally {
+            setUploading(false);
+        }
+    }
+
+    return (
+        <div>
+            <input type="file" accept="image/*" onChange={uploadImage} disabled={uploading} />
+            {uploading && <p>Uploading...</p>}
+        </div>
+    )
+}
