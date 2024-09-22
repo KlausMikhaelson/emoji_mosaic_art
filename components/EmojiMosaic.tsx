7c382d8e8aad6d72787c8d4453aa598ff71faf74
KlausMikhaelson <satyamsingh5076@gmail.com>
2024-09-22 02:57:32 -0600

    added option to change background color

--- components/EmojiMosaic.tsx
+++ components/EmojiMosaic.tsx
@@ -3,6 +3,7 @@ import html2canvas from "html2canvas";
 
 export default function EmojiMosaic({ mosaic, gridSize = 128, backgroundColor = "black" }: any) {
   const mosaicRef = useRef<HTMLDivElement>(null);
+  const [newBgColor, setNewBgColor] = React.useState(backgroundColor);
 
   const handleDownload = async () => {
     if (mosaicRef.current) {
@@ -12,7 +13,7 @@ export default function EmojiMosaic({ mosaic, gridSize = 128, backgroundColor = 
         logging: true,
         width: mosaicRef.current.scrollWidth,
         height: mosaicRef.current.scrollHeight,
-        backgroundColor: backgroundColor,
+        backgroundColor: newBgColor,
       });
       const link = document.createElement("a");
       link.href = canvas.toDataURL("image/png");
@@ -27,13 +28,17 @@ export default function EmojiMosaic({ mosaic, gridSize = 128, backgroundColor = 
     gap: "1px",
     padding: "10px",
     borderRadius: "5px",
-    backgroundColor: backgroundColor, // Set the background color for the mosaic
+    backgroundColor: newBgColor, // Set the background color for the mosaic
   };
 
   return (
     <div className="flex m-4 flex-col items-center justify-center">
-        <button className="bg-black px-4 py-3 rounded-md hover:text-gray-400 border-[1px] border-gray-400" onClick={handleDownload}>Download Mosaic</button>
-      <div ref={mosaicRef} style={style} className="h-60 w-fit">
+      <div className="flex items-center">
+        <label htmlFor="color" className="text-2xl">Change background color</label>
+      <input type="color" value={newBgColor} onChange={(e) => setNewBgColor(e.target.value)} className="rounded-md m-4 w-16 cursor-pointer"/>
+      </div>
+        <button className="bg-black px-4 py-3 rounded-md hover:text-gray-400 border-[1px] border-gray-400 m-4" onClick={handleDownload}>Download Mosaic</button>
+      <div ref={mosaicRef} style={style} className="h-auto w-fit">
         {mosaic.map((cell: any, index: any) => (
           <div key={index} style={{ fontSize: "4px" }}>
             {cell.emoji}
